161f7bb026365fa994352a997a0b7efbb2cd56ab
Julien Chaumond <chaumond@gmail.com>
2013-12-05 15:21:31 +0100

    Serialize selection range position

--- public/javascripts/reader.js
+++ public/javascripts/reader.js
@@ -9,4 +9,39 @@ document.onkeydown = function(e) {
 			window.location = '/' + componentNext.href;
 		}
 	}
+	else if (e.which == 27) {
+		window.location = '/';
+	}
 };
+
+
+document.onmouseup = function(e) {
+	var selection = window.getSelection();
+	if (selection.toString()) {
+		var range = selection.getRangeAt(0);
+		var segment = {
+			position: getPathTo(range.startContainer) + ':' + range.startOffset + ',' + getPathTo(range.endContainer) + ':' + range.endOffset,
+			text: range.toString()
+		};
+		console.log(segment);
+	}
+};
+
+
+
+function getPathTo(element) {
+	if (element.tagName == undefined) {
+		return getPathTo(element.parentNode);
+	}	
+	var siblings = element.parentNode.childNodes;
+	for (var i = 0; i < siblings.length; i++) {
+		if (siblings[i] === element) {
+			if (element.parentNode.tagName == 'BODY') {
+				return i;
+			}
+			else {
+				return getPathTo(element.parentNode) + '/' + i;
+			}
+		}
+	}
+}
